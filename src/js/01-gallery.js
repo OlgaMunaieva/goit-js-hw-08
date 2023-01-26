1aad253ad902136664e124509bbf00635e54ff95
OlgaMunaieva <asyssync@gmail.com>
2023-01-26 13:43:01 +0200

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,74 +10,19 @@ console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
 
-const markupGalleryPictures = galleryItems
+const makeupGalleryPictures = galleryItems
   .map(
     ({ preview, original, description }) =>
-      `<div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
   )
   .join('');
 
-galleryEl.innerHTML = markupGalleryPictures;
+galleryEl.innerHTML = makeupGalleryPictures;
 
-galleryEl.addEventListener('click', showLargePicture);
-
-/**
- * the function removes the default link actions, gets the address of a large image, creates an active image and displays it on the screen, adds closing the image on pressing the "Esc" button
- * @param {Object} event from EventListener
- */
-function showLargePicture(event) {
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  event.preventDefault();
-
-  const ImgSource = event.target.dataset.source;
-
-  const activePicture = createActivePicture(ImgSource);
-
-  activePicture.show();
-
-  closePictureButtonEsc(activePicture);
-}
-
-/**
- * the function creates HTML markup for active picture
- * @param {String} img address of the picture from function getImgSource
- * @returns string with HTML markup
- */
-function createActivePicture(img) {
-  return basicLightbox.create(
-    `
-    <img src="${img}" width="800" height="600">
-`
-  );
-}
-
-/**
- * the function closes the active picture if the button "Esc" is pressed and there is an active picture on the screen, it works once
- * @param {object} activePicture from the function createActivePicture
- */
-function closePictureButtonEsc(activePicture) {
-  document.addEventListener(
-    'keydown',
-    event => {
-      if (event.code !== 'Escape') {
-        return;
-      } else if (!basicLightbox.visible()) {
-        return;
-      } else {
-        activePicture.close();
-      }
-    },
-    { once: true }
-  );
-}
+const activePicture = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+activePicture.on('show.simplelightbox');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+// import Player from '@vimeo/player';
 
+const iframeEl = document.getElementById('vimeo-player');
+console.log(iframeEl);
+
+iframeEl.insertAdjacentHTML(
+  'afterend',
+  `<script>import Player from '@vimeo/player';</script><script src="https://player.vimeo.com/api/player.js"></script>
+<script>
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+</script>`
+);
