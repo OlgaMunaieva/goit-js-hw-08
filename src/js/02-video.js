203a2cdd510cc94bb1907c8d140be425faf9e6b9
OlgaMunaieva <asyssync@gmail.com>
2023-01-28 09:17:02 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,48 +1,24 @@
 import Player from '@vimeo/player';
-import lodash from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const CURRENT_TIME = 'videoplayer-current-time';
-
 const iframeEl = document.getElementById('vimeo-player');
-console.log(iframeEl);
-
 const player = new Player(iframeEl);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('play', () => console.log('played the video!'));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.getVideoTitle().then(title => console.log('title:', title));
 
 player.on(
   'timeupdate',
-  () => {
-    player.getCurrentTime().then(function (seconds) {
-      // seconds = the current playback position
-      console.log(seconds);
-      localStorage.setItem(CURRENT_TIME, seconds);
-      const time = seconds;
-      console.log(time);
-      // player.setCurrentTime(Number(seconds));
-    });
-  },
-  1000
+  throttle(
+    () =>
+      player
+        .getCurrentTime()
+        .then(seconds => localStorage.setItem(CURRENT_TIME, seconds)),
+    1000
+  )
 );
 
-function getCurrentTime() {
-  player.getCurrentTime().then(function (seconds) {
-    // seconds = the current playback position
-    console.log(seconds);
-    localStorage.setItem(CURRENT_TIME, seconds);
-    const time = seconds;
-    console.log(time);
-    // player.setCurrentTime(Number(seconds));
-  });
-  // console.log(currentTime);
-}
-
-// console.log(currentTime);
 console.log(localStorage.getItem(CURRENT_TIME));
 player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
