28c6aaee6c4986ce2e0ca0b4baffa18fa8f972f4
OlgaMunaieva <asyssync@gmail.com>
2023-01-28 11:28:36 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,20 +5,31 @@ const CURRENT_TIME = 'videoplayer-current-time';
 const iframeEl = document.getElementById('vimeo-player');
 const player = new Player(iframeEl);
 
-player.on('play', () => console.log('played the video!'));
+// player.on('play', () => console.log('played the video!'));
+// player.getVideoTitle().then(title => console.log('title:', title));
 
-player.getVideoTitle().then(title => console.log('title:', title));
+keepPlayingTime();
 
-player.on(
-  'timeupdate',
-  throttle(
-    () =>
-      player
-        .getCurrentTime()
-        .then(seconds => localStorage.setItem(CURRENT_TIME, seconds)),
-    1000
-  )
-);
-
-console.log(localStorage.getItem(CURRENT_TIME));
 player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+
+/**
+ * gets and records the current playing time locally at 1 second intervals
+ */
+function keepPlayingTime() {
+  player.on(
+    'timeupdate',
+    throttle(
+      () =>
+        player
+          .getCurrentTime()
+          .then(seconds => localStorage.setItem(CURRENT_TIME, seconds)),
+      1000
+    )
+  );
+}
+
+// function setPlayingTimeLocal(time) {
+//   localStorage.setItem(CURRENT_TIME, time);
+// }
+
+// console.log(localStorage.getItem(CURRENT_TIME));
